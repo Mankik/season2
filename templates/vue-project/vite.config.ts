4569b7920bacd91cbe088b42f8bce2861aa1a482
79gun79 <79gun79@gmail.com>
2024-03-11 16:21:50 +0900

    delete file

--- templates/vue-project/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import type { ConfigEnv } from "vite";
-import { VueConfig } from "vite-config-custom";
-
-// https://vitejs.dev/config/
-export default (viteConfigEnv: ConfigEnv) => {
-  return VueConfig(viteConfigEnv);
-};
